51216f12b0b5256b29fb3c2fc6982c06ba0a8850
sharon900 <mwangisharon@gmail.com>
2024-04-02 02:52:57 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+  String greeting = 'Hello,';
+  String name = 'John';
+  int age = 30;
+
+  print('$greeting $name!'); // Output: Hello, John!
+  print('You are $age years old.'); // Output: You are 30 years old.
+
+  String multiLineString = '''
+  Dart is a 
+  powerful language 
+  for building mobile, web, and desktop applications.
+  ''';
+  print(multiLineString); 
+}
